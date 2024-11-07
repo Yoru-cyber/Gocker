a2e05ed222f488f1f8f1a9ea489d63dc313c7014
Yoru-cyber <carlosmendez170210@gmail.com>
2024-11-06 22:37:34 -0400

    added more material design colors, improved visuals on container route

--- Gocker-Client/src/routes/__root.tsx
+++ Gocker-Client/src/routes/__root.tsx
@@ -4,18 +4,26 @@ import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
+      <div className="p-2 flex space-x-2 border-b-2 border-[var(--outline-variant)]">
+        <Link
+          to="/"
+          className="[&.active]:font-bold [&.active]:text-[var(--secondary)]"
+        >
           Home
         </Link>{" "}
-        <Link to="/container" className="[&.active]:font-bold">
+        <Link
+          to="/container"
+          className="[&.active]:font-bold [&.active]:text-[var(--secondary)]"
+        >
           Containers
         </Link>
-        <Link to="/about" className="[&.active]:font-bold">
+        <Link
+          to="/about"
+          className="[&.active]:font-bold [&.active]:text-[var(--secondary)]"
+        >
           About
         </Link>
       </div>
-      <hr />
       <Outlet />
       <TanStackRouterDevtools />
     </>
--- Gocker-Client/src/routes/container/index.tsx
+++ Gocker-Client/src/routes/container/index.tsx
@@ -23,37 +23,64 @@ function Index() {
   if (status == "pending") return "Loading...";
 
   if (status == "error") return "An error has occurred: " + status;
+
   return (
     <>
-      <div className="p-2">
-        <h3 className="text-3xl">
-          Containers: {Array.isArray(data) && data.length}
-        </h3>
-      </div>
-      <div>
-        {Array.isArray(data) &&
-          data.map((container: IContainer) => (
-            <div
-              id={container.Id}
-              key={container.Id}
-              className="bg-[var(--complementary-dark)] space-y-2 mb-5 rounded-md w-fit p-5"
-            >
-              <h2>Container Details</h2>
-              <p>ID: {container.Id}</p>
-              <p>Names: {container.Names[0]}</p>
-              <p>State: {container.State}</p>
-              <p>Status: {container.Status}</p>
-              <p>Image: {container.Image}</p>
-              <Link
-                className=" rounded-md italic text-[var(--primary)]"
-                to="/container/$containerId"
-                params={{ containerId: container.Id }}
+      <main className="w-screen h-screen">
+        <div className="p-2">
+          <h3 className="text-3xl font-semibold">
+            Currently running{" "}
+            <b className="text-[var(--tertiary)]">
+              {Array.isArray(data) && data.length}
+            </b>{" "}
+            Containers
+          </h3>
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 m-2 justify-items-center">
+          {Array.isArray(data) &&
+            data.map((container: IContainer) => (
+              <div
+                id={container.Id}
+                key={container.Id}
+                className="bg-[var(--complementary-dark)] space-y-2 mb-5 rounded-md w-fit p-5 border-2 border-[var(--outline-variant)]"
               >
-                Details
-              </Link>
-            </div>
-          ))}
-      </div>
+                <p className="text-xl font-semibold text-center">
+                  {container.Names[0]}
+                </p>
+                <p>ID: {container.Id}</p>
+                <p>
+                  State:
+                  <p
+                    style={{
+                      color:
+                        container.State === "running"
+                          ? "green"
+                          : container.State === "stopped"
+                            ? "red"
+                            : "black",
+                      display: "inline",
+                      marginLeft: "0.5em",
+                    }}
+                  >
+                    {container.State.charAt(0).toUpperCase() +
+                      container.State.slice(1)}
+                  </p>
+                </p>
+                <p>Status: {container.Status}</p>
+                <p>Image: {container.Image}</p>
+                <span className="inline-flex justify-center w-full">
+                  <Link
+                    className=" rounded-md italic text-[var(--primary)] transition-all hover:text-[var(--primary-variant-3)]"
+                    to="/container/$containerId"
+                    params={{ containerId: container.Id }}
+                  >
+                    Details
+                  </Link>
+                </span>
+              </div>
+            ))}
+        </div>
+      </main>
     </>
   );
 }
